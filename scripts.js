bcbd3f5cd27cc35ccb4d50df48f8663b84829c44
LucasM <lucassss8v@gmail.com>
2022-06-21 13:53:46 -0300

    Finished messaging page layout

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+let userName;
+
 function getName(){
+    userName = document.querySelector(".login-Name").value;
+    console.log(userName)
+    hideLogin()
     
 }
+
+function hideLogin(){
+    if (userName === ""){
+        alert("Por favor digite um nome")
+    }
+    else {
+        let loginPage = document.querySelector(".login-page");
+        loginPage.classList.add("hidden")
+        let messagePage = document.querySelector(".container")
+        messagePage.classList.remove("hidden")
+    }
+}
+    
